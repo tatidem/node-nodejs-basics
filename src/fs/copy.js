3712b735d83e2d8bce84c7affdb57f51085c8737
tatidem <tdemyanova@mail.ru>
2024-10-06 20:33:54 +0400

    feat: add create and copy functions

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,56 @@
+import { promises as fs } from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const copy = async () => {
-    // Write your code here 
+    const srcDir = path.join(__dirname, 'files');
+    const destDir = path.join(__dirname, 'files_copy');
+
+    try {
+        await fs.stat(srcDir);
+        try {
+            await fs.stat(destDir);
+            throw new Error('FS operation failed');
+        } catch (error) {
+            if (error.code !== 'ENOENT') {
+                throw error;
+            }
+        }
+        await fs.mkdir(destDir);
+        const entries = await fs.readdir(srcDir, { withFileTypes: true });
+        for (const entry of entries) {
+            const srcPath = path.join(srcDir, entry.name);
+            const destPath = path.join(destDir, entry.name);
+
+            if (entry.isDirectory()) {
+                await fs.mkdir(destPath);
+                await copyFolder(srcPath, destPath);
+            } else {
+                await fs.copyFile(srcPath, destPath);
+            }
+        }
+        console.log('Folder copied successfully');
+    } catch (error) {
+        console.error('Error:', error.message);
+    }
+};
+
+const copyFolder = async (src, dest) => {
+    const entries = await fs.readdir(src, { withFileTypes: true });
+    for (const entry of entries) {
+        const srcPath = path.join(src, entry.name);
+        const destPath = path.join(dest, entry.name);
+
+        if (entry.isDirectory()) {
+            await fs.mkdir(destPath);
+            await copyFolder(srcPath, destPath);
+        } else {
+            await fs.copyFile(srcPath, destPath);
+        }
+    }
 };
 
 await copy();
