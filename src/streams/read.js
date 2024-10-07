2be0df4e21d46e7bdba503502c2a561a02dfb93d
tatidem <tdemyanova@mail.ru>
2024-10-07 15:33:51 +0400

    feat: add stream functions

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,18 @@
+import { createReadStream } from 'fs';
+import { fileURLToPath } from 'url';
+import { dirname, join } from 'path';
+
 const read = async () => {
-    // Write your code here 
+    const __filename = fileURLToPath(import.meta.url);
+    const __dirname = dirname(__filename);
+    const filePath = join(__dirname, 'files', 'fileToRead.txt');
+    const stream = createReadStream(filePath);
+
+    stream.pipe(process.stdout);
+
+    stream.on('error', (err) => {
+        console.error('Error reading the file:', err);
+    });
 };
 
 await read();
--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,28 @@
+import { Transform } from 'stream';
+
 const transform = async () => {
-    // Write your code here 
+    const reverseStream = new Transform({
+        transform(chunk, _encoding, callback) {
+            const reversedChunk = chunk.toString().trim().split('').reverse().join('') + '\n';
+            callback(null, reversedChunk);
+        }
+    });
+
+    process.stdout.write('Enter your text hear and it would be reversed\n');
+
+    process.stdin.pipe(reverseStream).pipe(process.stdout);
+
+    process.stdin.on('data', (chunk) => {
+        if (chunk.toString().trim() === 'end') {
+            process.stdin.unpipe(reverseStream);
+            reverseStream.end();
+            console.log('\nProcess ended by user input.');
+        }
+    });
+
+    reverseStream.on('error', (err) => {
+        console.error('Error in transform stream:', err);
+    });
 };
 
 await transform();
